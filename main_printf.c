3e21eca0f368259bb7828c67e595a62dc439f914
Guillaume Vardakis <gvardaki@shi-r11-p5.s19.be>
2023-04-24 16:07:11 +0200

    %d and %i

--- main_printf.c
+++ main_printf.c
@@ -6,7 +6,7 @@
 /*   By: gvardaki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 18:00:02 by gvardaki          #+#    #+#             */
-/*   Updated: 2023/04/24 15:21:14 by gvardaki         ###   ########.fr       */
+/*   Updated: 2023/04/24 15:31:51 by gvardaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,16 @@
 int	main(void)
 {
 	int		ret;
+	int		ft_ret;
 	char	c;
 	char	*str = "string";
 	int		nbr = 2147483647;
 	
 	ret = 0;
+	ft_ret = 0;
 	c = 'a';
-	ret = ft_printf("\nBonjo%%ur char = %c\n str = %s\n p = %p\n decimal = %d\n int = %i\n", c, str, &c, nbr, nbr);
-	printf("ft_ret = %d\n", ret);
+	ft_ret = ft_printf("\nBonjo%%ur char = %c\n str = %s\n p = %p\n decimal = %d\n int = %i\n", c, str, &c, nbr, nbr);
+	printf("ft_ret = %d\n", ft_ret);
 	ret = printf("\nBonjo%%ur char = %c\n str = %s\n p = %p\n decimal = %d\n int = %i\n", c, str, &c, nbr, nbr);
 	printf("ret = %d\n", ret);
 	return (0);
